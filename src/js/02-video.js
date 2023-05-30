a122c8a9e0f91a16d8e45bd7d7ee2e659a982314
SonGlad <okoshevy@gmail.com>
2023-05-30 13:26:32 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,41 @@
-import Player from '@vimeo/player';
-import Throttle from 'lodash.throttle';
+import Player from '@vimeo/player'; 
+import Throttle from 'lodash.throttle'
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const STORAGE_KEY = 'videoplayer-current-time';
+const STORAGE_KEY = "videoplayer-current-time";
 
-player.on('timeupdate', Throttle(function ({ seconds }) {
-    localStorage.setItem(STORAGE_KEY, seconds);
-  }, 1000)
-);
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+
+player.on('timeupdate', Throttle(function({seconds}) { 
+  localStorage.setItem(STORAGE_KEY, seconds)
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function({seconds}) {
+  // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+  switch (error.name) {
+    case 'RangeError':
+      // the time was less than 0 or greater than the video’s duration
+      break;
+      
+      default:
+        // some other error occurred
+        break;
+      }
+    });
+
+
+
+
+    // import Player from '@vimeo/player';
+    // import Throttle from 'lodash.throttle';
+    
+    // const iframe = document.querySelector('iframe');
+    // const player = new Player(iframe);
+    // const STORAGE_KEY = 'videoplayer-current-time';
+    
+    // player.on('timeupdate', Throttle(function ({ seconds }) {
+    //     localStorage.setItem(STORAGE_KEY, seconds);
+    //   }, 1000)
+    // );
+    // player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
