a87991c47a184b20387218fd3a3fc425fdf9a6b3
SonGlad <okoshevy@gmail.com>
2023-06-30 12:02:22 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,32 +1,32 @@
-// Add imports below this line
-import {galleryItems} from './gallery-items';
-import SimpleLightbox from "simplelightbox";
+// // Add imports below this line
+// import {galleryItems} from './gallery-items';
+// import SimpleLightbox from "simplelightbox";
 
-// Extra styles import
+// // Extra styles import
 
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
-// Change code below this line
+// // Change code below this line
 
-const refs = {
-  ulEl: document.querySelector(".gallery")
-};
-refs.ulEl.style.listStyle = "none";
+// const refs = {
+//   ulEl: document.querySelector(".gallery")
+// };
+// refs.ulEl.style.listStyle = "none";
 
-const galleryOfItems = creatGalleryItems(galleryItems);
-refs.ulEl.insertAdjacentHTML("beforeend", galleryOfItems);
+// const galleryOfItems = creatGalleryItems(galleryItems);
+// refs.ulEl.insertAdjacentHTML("beforeend", galleryOfItems);
 
-function creatGalleryItems(galleryItems){
-  return galleryItems.map(({preview, original, description}) => {
-    return `<li class="gallery__item">
-              <a class="gallery__link" href="${original}">
-                <img class="gallery__image" 
-                src="${preview}" 
-                alt="${description}"/></a></li>`
-  }).join('');
-};
+// function creatGalleryItems(galleryItems){
+//   return galleryItems.map(({preview, original, description}) => {
+//     return `<li class="gallery__item">
+//               <a class="gallery__link" href="${original}">
+//                 <img class="gallery__image" 
+//                 src="${preview}" 
+//                 alt="${description}"/></a></li>`
+//   }).join('');
+// };
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 300,
-});
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 300,
+// });
